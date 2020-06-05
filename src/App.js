80e14427a8a899c55fde674b4f2ce3392834ae6a
Ride0401 <devarata.oza@gmail.com>
2020-06-06 03:27:04 +0530

    Drag and drop functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import DropZone from './dropzone/Dropzone';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="body">
             <p className="title">React Drag and Drop Image Upload</p>
             <div className="content">
-
+              <DropZone/>
             </div>
         </div>
   );
