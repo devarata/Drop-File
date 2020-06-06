a92d23b6b92d109c9d6f826a540530f172d017f1
Ride0401 <devarata.oza@gmail.com>
2020-06-06 14:24:50 +0530

    DropFile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="body">
             <p className="title">React Drag and Drop Image Upload</p>
             <div className="content">
